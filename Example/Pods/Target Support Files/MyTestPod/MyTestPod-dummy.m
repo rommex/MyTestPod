877e442d07c334272adaec810fef09d89f919d3a
Roman Medvid <dev@kadru.net>
2020-09-24 14:48:02 +0300

    init

---
+++ Example/Pods/Target Support Files/MyTestPod/MyTestPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyTestPod : NSObject
+@end
+@implementation PodsDummy_MyTestPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyTestPod_Tests/Pods-MyTestPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyTestPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyTestPod_Tests
+@end
